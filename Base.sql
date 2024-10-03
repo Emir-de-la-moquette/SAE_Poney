9b10d4398237396ea8d03dcb82d3491e47525ba5
Emir-de-la-moquette <shanky.yt@gmail.com>
2024-10-03 12:11:33 +0200

    aajout bd

--- Base.sql
+++ Base.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE PERSONNE (
-    idPers SERIAL PRIMA,
+    idPers SERIAL PRIMARY KEY,
     nomPers VARCHAR(64),
     prenomPers VARCHAR(64),
     poids INT,
@@ -9,14 +9,55 @@ CREATE OR REPLACE PERSONNE (
 );
 
 CREATE OR REPLACE ENCADRANT (
-    idPers SERIAL,
+    idPers INT PRIMARY KEY,
     nbHeuresMax INT,
-    PRIMARY KEY (idPers)
-)
+    FOREIGN KEY (idPers) REFERENCES personne(idPers);
+);
+
+CREATE OR REPLACE CLIENT (
+    idPers INT PRIMARY KEY,
+    dateInscription DATE,
+    FOREIGN KEY (idPers) REFERENCES personne(idPers);
+);
+
+CREATE OR REPLACE COTISATION_CLIENT (
+    idPers INT,
+    anneecotisation DATE,
+    prix DECIMAL(6,2),
+    PRIMARY KEY(idPers, anneecotisation),
+    FOREIGN KEY (idPers) REFERENCES personne(idPers);
+);
 
 CREATE OR REPLACE PONEY (
-    idPoney SERIAL,
+    idPoney SERIAL PRIMARY KEY,
     nomPoney VARCHAR(32),
     poidsMax INT,
     tailleMax INT;
+);
+
+CREATE OR REPLACE SEANCE (
+    idSeance SERIAL PRIMARY KEY,
+    intitule VARCHAR(64),
+    duree INT,
+    jma DATE,
+    hh INT;
+);
+
+CREATE OR REPLACE COURS (
+    idCours SERIAL PRIMARY KEY,
+    nbPersonneMax INT,
+    nomCours VARCHAR(64);
+);
+
+CREATE OR REPLACE NIVEAU (
+    niveau INT PRIMARY KEY,
+);
+
+CREATE TABLE OBTENIR_LVL (
+    idPers INT,
+    niveau INT,
+    jma DATE, 
+    PRIMARY KEY (idPers, idNiveau, jma),
+    FOREIGN KEY (idPers) REFERENCES personne(idPers),
+    FOREIGN KEY (idNiveau) REFERENCES niveau(idNiveau);
 );
