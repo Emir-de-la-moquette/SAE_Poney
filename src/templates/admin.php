bae899c2e9f7627a6f8f9977bc5882005d3948db
TristanChaloine <tristan.chaloine@etu.univ-orleans.fr>
2025-01-06 17:45:18 +0100

    affichage via la BD local de l'iut de Tristan

--- src/templates/admin.php
+++ src/templates/admin.php
@@ -1,3 +1,13 @@
+<?php
+    $dsn = "mysql:dbname="."DBchaloine".";host="."servinfo-maria";
+    try{
+        $connexion = new PDO($dsn, "chaloine", "chaloine");
+    }
+    catch(PDOException $e){
+        printf("Error connecting to database: %s", $e->getMessage());
+        exit();
+    }
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -31,21 +41,24 @@
             <section class="section">
                 <div class="table-container">
                     <h2>Gestion moniteurs</h2>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Nom moniteurs</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr><td>Menvusa Gérard</td><td>...</td></tr>
-                            <tr><td>Laden Ben</td><td>...</td></tr>
-                            <tr><td>Terrieur Alex</td><td>...</td></tr>
-                            <tr><td>Terrieur Alain</td><td>...</td></tr>
-                            <tr><td>Laporte Jean-Phonse</td><td>...</td></tr>
-                        </tbody>
-                    </table>
+                        <?php
+                        $sql = "SELECT * FROM PERSONNE NATURAL JOIN ENCADRANT where idPers = idEnc";
+                        if (!$connexion->query($sql)){
+                            echo "Error: %s\n";
+                        }
+                        else{
+                            $result = $connexion->query($sql);
+                            echo "<table border='1'>";
+                            echo "<tr><th>Nom</th><th>Prénom</th></tr>";
+                            foreach($result as $row){
+                                echo "<tr>";
+                                echo "<td>".$row['nomPers']."</td>";
+                                echo "<td>".$row['prenomPers']."</td>";
+                                echo "</tr>";
+                            }
+                            echo "</table>";
+                        }
+                        ?>
                     <button>Ajouter un moniteur +</button>
                 </div>
             </section>
@@ -53,26 +66,28 @@
             <section class="section">
                 <div class="table-container">
                     <h2>Gestion adhérents</h2>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Nom client</th>
-                                <th>Date insc.</th>
-                                <th>Payé</th>
-                                <th>Cours payé</th>
-                                <th>Poids</th>
-                                <th>Taille</th>
-                                <th>Niveau</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr><td>001</td><td>Dure Laure</td><td>2001/09/11</td><td>🟢</td><td>🟢</td><td>69kg</td><td>180cm</td><td>exp</td></tr>
-                            <tr><td>002</td><td>Boula Alban</td><td>2023/09/17</td><td>🔴</td><td>🟢</td><td>80kg</td><td>240cm</td><td>int</td></tr>
-                            <tr><td>003</td><td>Pheure kwoï</td><td>2021/09/03</td><td>🔴</td><td>🔴</td><td>23kg</td><td>172cm</td><td>int</td></tr>
-                            <tr><td>004</td><td>White Walter</td><td>2022/09/04</td><td>🟢</td><td>🟢</td><td>59kg</td><td>112cm</td><td>deb</td></tr>
-                        </tbody>
-                    </table>
+                    <?php
+                    $sql = "SELECT * FROM PERSONNE NATURAL JOIN CLIENT where idPers = idCli";
+                    if (!$connexion->query($sql)){
+                        echo "Error: %s\n";
+                    }
+                    else{
+                        $result = $connexion->query($sql);
+                        echo "<table border='1'>";
+                        echo "<tr><th>Nom</th><th>Prénom</th><th>Tel</th><th>Mail</th><th>Taille</th><th>Poids</th></tr>";
+                        foreach($result as $row){
+                            echo "<tr>";
+                            echo "<td>".$row['nomPers']."</td>";
+                            echo "<td>".$row['prenomPers']."</td>";
+                            echo "<td>".$row['tel']."</td>";
+                            echo "<td>".$row['mail']."</td>";
+                            echo "<td>".$row['taille']."</td>";
+                            echo "<td>".$row['poids']."</td>";
+                            echo "</tr>";
+                        }
+                        echo "</table>";
+                    }
+                    ?>
                     <button>Ajouter un adhérent +</button>
                 </div>
             </section>
@@ -80,27 +95,33 @@
             <section class="section">
                 <div class="table-container">
                     <h2>Gestion poneys</h2>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Nom animal</th>
-                                <th>Race</th>
-                                <th>Poids max</th>
-                                <th>Taille min</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr><td>001</td><td>Applejack</td><td>Poney</td><td>150kg</td><td>120cm</td></tr>
-                            <tr><td>002</td><td>Rarity</td><td>Licorne</td><td>80kg</td><td>140cm</td></tr>
-                            <tr><td>003</td><td>Twilight Sparkle</td><td>Alicorne</td><td>100kg</td><td>110cm</td></tr>
-                            <tr><td>004</td><td>Trixie</td><td>Licorne</td><td>110kg</td><td>120cm</td></tr>
-                        </tbody>
-                    </table>
+                    <?php
+                    $sql = "SELECT * FROM PONEY";
+                    if (!$connexion->query($sql)){
+                        echo "Error: %s\n";
+                    }
+                    else{
+                        $result = $connexion->query($sql);
+                        echo "<table border='1'>";
+                        echo "<tr><th>Nom</th><th>Poids maximal</th><th>Taille Mininimale</th><th>Race</th></tr>";
+                        foreach($result as $row){
+                            echo "<tr>";
+                            echo "<td>".$row['nomPoney']."</td>";
+                            echo "<td>".$row['poidsMax']."</td>";
+                            echo "<td>".$row['tailleMin']."</td>";
+                            echo "<td>".$row['nomRace']."</td>";
+                            echo "</tr>";
+                        }
+                        echo "</table>";
+                    }
+                    ?>
                     <button>Ajouter un poney +</button>
                 </div>
             </section>
         </main>
     </div>
+    <?php
+        include('footer.php');
+    ?>
 </body>
 </html>
