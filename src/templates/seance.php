eea512b1ef41e3b6fd5a17972cadc30c7fb08ab6
TronnoxUwU <baptisterichard98@free.fr>
2025-01-17 18:36:53 +0100

    fin

--- src/templates/seance.php
+++ src/templates/seance.php
@@ -1,3 +1,8 @@
+<?php require_once "../static/script/modele.php" ;
+    $info = getInfoSeance($_GET['id']);
+    $participants = getParticipants($_GET['id']);
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,29 +13,28 @@
 </head>
 <body>
     <aside class="sidebar">
-        <?php include "./navbar_admin.php" ?>
+        <?php //include "./navbar_admin.php" ?>
     </aside>
     <main>
         <div class="content">
-            <h1> Cours : <?php ?></h1>
-            <h2> Séance de <?php ?></h2>
+            <h1> <?php echo $info['nomCours']; ?></h1>
+            <h2> <?php echo $info['intitule']; ?></h2>
             <div>
-                <h3> Encadrant : <?php ?></h3>
+                <h3> Encadrant : <?php echo $info['prenomPers']." ".$info['nomPers']; ?></h3>
                 <div>
                 <h3> Participant :</h3>
                     <ul>
                         <?php
-                        $lesPersonnes = [['mail'=>'','nom'=>'Richard','prenom'=>'Baptiste']];
-                        foreach ($lesPersonnes as $personne) {
-                            echo "<li>".$personne['nom']." ".$personne['prenom']."</li>";
+                        foreach ($participants as $participant) {
+                            echo "<li>".$participant['nom']." ".$participant['prenom']."</li>";
                         }
                         ?>
                     </ul>
-                    <?php if(count($lesPersonnes)==10){echo "<p>Le cours est complé</p>";}?>
+                    <?php if(count($participants)==10){echo "<p>Le cours est complé</p>";}?>
                 </div>
             </div>
-            <?php if(count($lesPersonnes)<10 && !in_array($lesPersonnes,$_SESSION['user'])){
-                echo "<a href='/src/templates/reservation.php?id=%s'>Réserver</a>";
+            <?php if(count($participants)<=10){
+                printf("<a href='/src/templates/reservation.php?id=%s'>Réserver</a>",$_GET['id']);
             }
             ?>
         </div>
